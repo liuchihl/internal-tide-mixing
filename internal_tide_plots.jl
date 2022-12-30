c24bbc4ef9a8c6a635e3baa648cff31dc9d4c2fc
Henri Drake <henrifdrake@gmail.com>
2022-12-30 12:34:08 -0500

    Tilts internal tide example
    Tilts the simulation domain by an angle theta in the (x,z) plane, following https://github.com/CliMA/Oceananigans.jl/blob/086053242b666c77bdb1113be3c4671313b9147e/examples/tilted_bottom_boundary_layer.jl.
    The simulation solves only for the perturbations away from a
    constantly-stratified (in the true vertical) background buoyancy
    field, which are allowed to be periodic in the cross-slope
    direction x and the (unchanged) along-slope direction y.
    
    Consistency in diagnostics and plots relative to the untilted
    problem is maintained by defining new diagnostics for the
    total buoyancy (background plus perturbation) and the
    zonal and vertical velocities. Contours of total buoyancy appear
    tilted in (x,z) plots; eventually it would be nice to be able to tilt
    the domain itself when plotting diagnostics to match with
    intuition and allow more clear comparisons between simulations
    with different slopes.

--- internal_tide_plots.jl
+++ internal_tide_plots.jl
@@ -21,23 +21,28 @@ function nan_solid(x, z, u, mask)
     return u
 end
 
-saved_output_filename = "internal_tide_Yi-et-al-2017-40days.jld2"
+saved_output_filename = "internal_tide_tilted-test.jld2"
 file = jldopen(saved_output_filename)
 
 ig = file["serialized/grid"]
 ug = ig.underlying_grid
+ĝ = file["serialized/buoyancy"].gravity_unit_vector
 
-u = FieldTimeSeries(saved_output_filename, "u")
+u = FieldTimeSeries(saved_output_filename, "û")
+w = FieldTimeSeries(saved_output_filename, "ŵ")
 ε = FieldTimeSeries(saved_output_filename, "ε")
 b = FieldTimeSeries(saved_output_filename, "b")
 t = b.times
 
 xu, yu, zu = nodes(u[1])
+xw, yw, zw = nodes(w[1])
 xb, yb, zb = nodes(b[1])
 
-# transform ε to logspace
+B = deepcopy(b)
+N = 1.e-3
 for n in 1:length(t)
-    ε[n].data .= log10.(ε[n].data)
+    ε[n].data .= log10.(@. ifelse(ε[n].data > 0, ε[n].data, 1.e-100)) # transform ε to logspace
+    B[n].data .= b[n].data .+ N^2 * (xb[:,:,:]*ĝ[1] .+ reshape(zb, (1, 1, Nz))*ĝ[3])
 end
 
 n = Observable(1)
@@ -46,7 +51,7 @@ n = Observable(1)
 mask = interior(ig.immersed_boundary.mask)[:,1,:]
 uₙ = @lift nan_solid(xu, zu, interior(u[$n], :, 1, :), mask)
 εₙ = @lift nan_solid(xb, zb, interior(ε[$n], :, 1, :), mask)
-bₙ = @lift nan_solid(xb, zb, interior(b[$n], :, 1, :), mask)
+Bₙ = @lift nan_solid(xb, zb, interior(B[$n], :, 1, :), mask)
 
 begin
     fig = Figure(resolution = (1000, 1000), figure_padding=(10, 40, 10, 10))
@@ -54,25 +59,25 @@ begin
                       ylabel = "elevation (z)",
                       limits = ((0, ug.Lx), (0, ug.Lz)),
                       )
-    ax_u = Axis(fig[1, 1]; title = "TKE dissipation rate (ε) and equally-spaced buoyancy contours (b)", axis_kwargs...)
-    ax_ε = Axis(fig[2, 1]; title = "zonal velocity (u) and equally-spaced buoyancy contours (b)", axis_kwargs...)
+    ax_u = Axis(fig[1, 1]; title = "zonal velocity (u) and equally-spaced buoyancy contours (B)", axis_kwargs...)
+    ax_ε = Axis(fig[2, 1]; title = "TKE dissipation rate (ε) and equally-spaced buoyancy contours (B)", axis_kwargs...)
 end
 
 U₀ = 0.025
 hm_u = heatmap!(ax_u, xu, zu, uₙ,
     colorrange = (-3U₀, 3U₀), colormap = :balance,
     lowclip=cgrad(:balance)[1], highclip=cgrad(:balance)[end])
-ct_u = contour!(ax_u, xb, zb, bₙ,
-    levels=0.:0.5e-4:3.e-3, linewidth=0.25, color=:black, alpha=0.2)
+ct_u = contour!(ax_u, xb, zb, Bₙ,
+    levels=0.:0.5e-4:4.e-3, linewidth=0.25, color=:black, alpha=0.2)
 
 hm_ε = heatmap!(ax_ε, xb, zb, εₙ,
     colorrange = (-11, -8), colormap = :matter,
     lowclip=cgrad(:matter)[1], highclip=cgrad(:matter)[end])
-ct_ε = contour!(ax_ε, xb, zb, bₙ,
-    levels=0.:0.5e-4:3.e-3, linewidth=0.25, color=:black, alpha=0.2)
+ct_ε = contour!(ax_ε, xb, zb, Bₙ,
+    levels=0.:0.5e-4:4.e-3, linewidth=0.25, color=:black, alpha=0.2)
 
 frames = (1:length(t))
-record(fig, "test.mp4", frames, framerate=16) do i
+record(fig, string(split(saved_output_filename, ".")[1], ".mp4"), frames, framerate=16) do i
     @info "Plotting frame $i of $(frames[end])..."
     n[] = i
 end
--- tilted_internal_tide.jl
+++ tilted_internal_tide.jl
@@ -1,7 +1,6 @@
 using Printf
 using Oceananigans
 using Oceananigans.Units
-using Oceananigans.TurbulenceClosures: VerticallyImplicitTimeDiscretization
 using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBoundary
 using LinearAlgebra
 
@@ -17,7 +16,6 @@ tᶠ = 60minutes # simulation run time
 
 H = 3kilometers # 6.e3 # vertical extent
 L = 2H # 60.e3 # horizontal extent
-θ = 0.002
 
 n = 1 # topographic wave number
 h = 200meters # topographic height
@@ -50,42 +48,65 @@ topog_mask(x, y, z) = z < topog(x, y, z)
 grid_with_bumps = ImmersedBoundaryGrid(grid, GridFittedBoundary(topog_mask))
 
 # Environmental parameters
-N = 1.e-3
-f₀ = 0.53e-4
+N = 1.e-3 # Brunt-Väisälä buoyancy frequency
+f₀ = 0.53e-4 # Coriolis frequency
+θ = 0.1 # tilting of domain in (x,z) plane, in radians [for small slopes tan(θ)~θ]
+ĝ = (sin(θ), 0, cos(θ))
 
 # Tidal forcing
 U₀ = 0.025
 ω₀ = 1.4e-4
-tidal_forcing(x, y, z, t) = U₀*ω₀*cos(ω₀*t)
+u_tidal_forcing(x, y, z, t) = U₀*ω₀*cos(ω₀*t)*(+ĝ[3])
+w_tidal_forcing(x, y, z, t) = U₀*ω₀*cos(ω₀*t)*(-ĝ[1])
 
 s = sqrt((ω₀^2-f₀^2)/(N^2-ω₀^2))
 γ = h*π/(s*6kilometers)
 print("Wave steepness of γ=",round(γ, digits=3))
 
+# Rotate gravity vector
+buoyancy = Buoyancy(model = BuoyancyTracer(), gravity_unit_vector = ĝ)
+coriolis = ConstantCartesianCoriolis(f = f₀, rotation_axis = ĝ)
+
+# Linear background stratification (in ẑ)
+@inline ẑ(x, z, ĝ) = x*ĝ[1] .+ z*ĝ[3]
+@inline constant_stratification(x, y, z, t, p) = p.N² * ẑ(x, z, p.ĝ)
+B̄_field = BackgroundField(constant_stratification, parameters=(; ĝ, N² = N^2))
+
 model = NonhydrostaticModel(
     grid = grid_with_bumps,
     advection = WENO(),
-    closure = ScalarDiffusivity(VerticallyImplicitTimeDiscretization(), ν=1e-4, κ=1e-4),
+    buoyancy = buoyancy,
+    coriolis = coriolis,
+    forcing = (u = u_tidal_forcing, w = w_tidal_forcing),
+    closure = ScalarDiffusivity(; ν=1e-4, κ=1e-4),
     tracers = :b,
-    buoyancy = BuoyancyTracer(),
-    coriolis = FPlane(f=f₀),
-    forcing = (u = tidal_forcing,))
+    timestepper = :RungeKutta3,
+    background_fields = (; b=B̄_field),
+)
 
-# Linear stratification
-bᵢ(x, y, z) = N^2*z + 1e-9*rand()
+bᵢ(x, y, z) = 1e-9*rand()
 uᵢ(x, y, z) = 0.
 vᵢ(x, y, z) = U₀*(ω₀*f₀)/(ω₀^2 - f₀^2)
-set!(model, b=bᵢ, u=uᵢ, v=vᵢ)
+wᵢ(x, y, z) = 0.
+set!(model, b=bᵢ, u=uᵢ, v=vᵢ, w=wᵢ)
 
 ## Configure simulation
 Δt = (1/N)*0.03
 simulation = Simulation(model, Δt = Δt, stop_time = tᶠ)
 
 ## Diagnostics
+b = model.tracers.b
+B̄ = model.background_fields.tracers.b
+B = B̄ + b # total buoyancy field
+
 u, v, w = model.velocities
+û = @at (Face, Center, Center) u*ĝ[3] + w*ĝ[1] # true zonal velocity
+ŵ = @at (Center, Center, Face) w*ĝ[3] - u*ĝ[1] # true vertical velocity
+
 ν = model.closure.ν
 ε = Field(ν*(∂x(u)^2 + ∂x(v)^2 + ∂x(w)^2 + ∂y(u)^2 + ∂y(v)^2 + ∂y(w)^2 + ∂z(u)^2 + ∂z(v)^2 + ∂z(w)^2))
-custom_diags = (ε=ε,)
+
+custom_diags = (B=B, û=û, ŵ=ŵ, ε=ε,)
 all_diags = merge(model.velocities, model.tracers, custom_diags)
 
 fname = string("internal_tide_", suffix,".jld2")
