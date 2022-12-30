c7ac4dff344ac61555899a44b1e9fb3329b62773
Henri Drake <henrifdrake@gmail.com>
2022-12-29 21:36:57 -0500

    Diagnose TKE dissip, stretched grid, non-zero IC
    - Added an initial condition to momentum (just to y-velocity) to match the predicted inertial oscillation response to the tidal forcing. This kills the inertial oscillations that otherwise dominates the internal tide component of the wave field for the first few tidal cycles (following Nikurashin and Legg).
    - Output snapshots of TKE dissipation rate as a diagnostic (and display it instead of vertical velocity in the movie)
    - Use a stretched grid with higher resolution where it matters (borrowed from https://github.com/CliMA/Oceananigans.jl/blob/main/examples/tilted_bottom_boundary_layer.jl)

--- internal_tide_replicate-NL2010.jl
+++ internal_tide_replicate-NL2010.jl
@@ -1,29 +1,53 @@
 using Printf
 using Oceananigans
+using Oceananigans.Units
 using Oceananigans.TurbulenceClosures: VerticallyImplicitTimeDiscretization
 using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBoundary
 using LinearAlgebra
+using Plots
 
-## parameters
-Nt = 10
-H = 6.e3
-L = H # 60.e3
+suffix = "10-day-test"
 
-grid = RectilinearGrid(size=(256, 1, 256), 
+## Simulation parameters
+Nx = 256
+Ny = 1
+Nz = 128
+
+tᶠ = 10days # simulation run time
+Δtᵒ = 30minutes # interval for saving output
+
+H = 3kilometers # 6.e3 # vertical extent
+L = H # 60.e3 # horizontal extent
+
+## Create grid 
+# Creates a vertical grid with near-constant spacing `refinement * Lz / Nz` near the bottom:
+# "Warped" coordinate
+kwarp(k, N) = (N + 1 - k) / N
+# Linear near-surface generator
+ζ(k, N, refinement) = 1 + (kwarp(k, N) - 1) / refinement
+# Bottom-intensified stretching function
+Σ(k, N, stretching) = (1 - exp(-stretching * kwarp(k, N))) / (1 - exp(-stretching))
+
+# Generating function
+z_faces(k) = - H * (ζ(k, Nz, 1.8) * Σ(k, Nz, 10) - 1)
+
+grid = RectilinearGrid(size=(Nx, Ny, Nz), 
         x = (0, L),
         y = (0, L), 
-        z = (0, H),
+        z = z_faces,
         halo = (4,4,4),
         topology = (Periodic, Periodic, Bounded)
 )
 
-# Sinusoidal topography of height h and mode number 6
-h = 200
-n = 6
-sinusoidal_bumps(x, y, z) = z < h * (1 + cos(n*(2π*x/L + π)))/2 + 2grid.Δzᵃᵃᶜ # ensure boundary is immersed
+plot(grid.Δzᵃᵃᶜ[1:Nz], grid.zᵃᵃᶜ[1:Nz], xlim=(0, NaN))
+
+# Sinusoidal topography of height h and mode number n
+h = 200meters
+n = 3
+topog_mask(x, y, z) = z < h * (1 + cos(n*(2π*x/L + π)))/2 + 2minimum(grid.Δzᵃᵃᶜ)
 
 # Create immersed boundary grid
-grid_with_bumps = ImmersedBoundaryGrid(grid, GridFittedBoundary(sinusoidal_bumps))
+grid_with_bumps = ImmersedBoundaryGrid(grid, GridFittedBoundary(topog_mask))
 
 # Environmental parameters
 N = 1.e-3
@@ -33,7 +57,6 @@ f₀ = 0.53e-4
 U₀ = 0.025
 ω₀ = 1.4e-4
 tidal_forcing(x, y, z, t) = U₀*ω₀*cos(ω₀*t)
-
 
 model = NonhydrostaticModel(
     grid = grid_with_bumps,
@@ -47,24 +70,31 @@ model = NonhydrostaticModel(
 # Linear stratification
 bᵢ(x, y, z) = N^2*z + 1e-9*rand()
 uᵢ(x, y, z) = 0.
-vᵢ(x, y, z) = 0.#U₀*(ω₀*f₀)/(ω₀^2 - f₀^2)
+vᵢ(x, y, z) = U₀*(ω₀*f₀)/(ω₀^2 - f₀^2)
 set!(model, b=bᵢ, u=uᵢ, v=vᵢ)
+
+## Configure simulation
+Δt = (1/N)*0.03
+simulation = Simulation(model, Δt = Δt, stop_time = tᶠ)
+
+## Diagnostics
+u, v, w = model.velocities
+ν = model.closure.ν
+ε = Field(ν*(∂x(u)^2 + ∂x(v)^2 + ∂x(w)^2 + ∂y(u)^2 + ∂y(v)^2 + ∂y(w)^2 + ∂z(u)^2 + ∂z(v)^2 + ∂z(w)^2))
+custom_diags = (ε=ε,)
+all_diags = merge(model.velocities, model.tracers, custom_diags)
+
+fname = string("internal_tide_NL2010_", suffix,".jld2")
+simulation.output_writers[:fields] = JLD2OutputWriter(model, all_diags,
+                                        schedule = TimeInterval(Δtᵒ),
+                                        filename = fname,
+                                        overwrite_existing = true)
+## Progress messages
 
 progress_message(s) = @info @sprintf("[%.2f%%], iteration: %d, time: %.3f, max|w|: %.2e",
                             100 * s.model.clock.time / s.stop_time, s.model.clock.iteration,
                             s.model.clock.time, maximum(abs, model.velocities.w))
-
-Δt = 0.02/N
-
-simulation = Simulation(model, Δt = Δt, stop_iteration = Nt)
-
-fname = string("internal_tide_NL2010_testing.jld2")
-simulation.output_writers[:fields] = JLD2OutputWriter(model, merge(model.velocities, model.tracers),
-                                                        schedule = IterationInterval(1),
-                                                        filename = fname,
-                                                        overwrite_existing = true)
-
-simulation.callbacks[:progress] = Callback(progress_message, IterationInterval(1))
+simulation.callbacks[:progress] = Callback(progress_message, TimeInterval(Δtᵒ))
 
 run!(simulation)
 
@@ -85,9 +115,9 @@ end
 
 function nan_solid(x, z, u, topog)
     Nx, Nz = size(u)
-    x2 = reshape(x, Nx, 1)
-    z2 = reshape(z, 1, Nz)
-    u[topog.(x2, 0, z2)] .= NaN
+    x2 = reshape(x, (Nx, 1))
+    z2 = reshape(z, (1, Nz))
+    u[topog.(x2, 0., z2)] .= NaN
     return nothing
 end
 
@@ -111,27 +141,26 @@ function visualize_internal_tide_simulation(prefix)
         @info "Plotting iteration $iter of $(iterations[end])..."
 
         u = file["timeseries/u/$iter"][:, 1, :]
-        w = file["timeseries/w/$iter"][:, 1, :]
         b = file["timeseries/b/$iter"][:, 1, :]
+        ε = file["timeseries/ε/$iter"][:, 1, :]
         t = file["timeseries/t/$iter"]
 
         b′ = b .- b₀
 
-        ulims, ulevels = nice_divergent_levels(u, 2*U0)
-        wlims, wlevels = nice_divergent_levels(w, U0/5)
+        ulims, ulevels = nice_divergent_levels(u, 2*U₀)
         blims, blevels = nice_divergent_levels(b′, N^2*grid.Lz/1000.)
         
-        nan_solid(xu, zu, u, sinusoidal_bumps)
-        nan_solid(xw, zw, w, sinusoidal_bumps)
-        nan_solid(xb, zb, b, sinusoidal_bumps)
+        nan_solid(xu, zu, u, topog_mask)
+        nan_solid(xb, zb, ε, topog_mask)
+        nan_solid(xb, zb, b, topog_mask)
 
         u_title = @sprintf("x velocity, t = %.2f", t)
 
         u_plot = contourf(xu, zu, u'; title = u_title,                  color = :balance, linewidth = 0, levels = ulevels, clims = ulims)
-        w_plot = contourf(xw, zw, w'; title = "z velocity",             color = :balance, linewidth = 0, levels = wlevels, clims = wlims)
-        b_plot = contourf(xb, zb, b′'; title = "buoyancy perturbation", color = :balance, linewidth = 0, levels = blevels, clims = blims)
+        ε_plot = contourf(xb, zb, log10.(ε)'; title = "dissipation",    color = :matter, linewidth = 0, clims = (-11., -7.))
+        b_plot = contourf(xb, zb, b′'; title = "buoyancy perturbation", color = :delta, linewidth = 0, levels = blevels, clims = blims)
 
-        plot(u_plot, w_plot, b_plot, layout = (3, 1), xlims=(0, L), ylims=(0, H), size = (1200, 1600), dpi=200)
+        plot(u_plot, b_plot, ε_plot, layout = (3, 1), xlims=(0, L), ylims=(0, H/2), size = (1200, 1600), dpi=200)
     end
 
     mp4(anim, prefix * ".mp4", fps = 20)
@@ -139,4 +168,4 @@ function visualize_internal_tide_simulation(prefix)
     close(file)
 end
 
-visualize_internal_tide_simulation(string("internal_tide_NL2010_testing"))
+visualize_internal_tide_simulation(string("internal_tide_NL2010_", suffix))
