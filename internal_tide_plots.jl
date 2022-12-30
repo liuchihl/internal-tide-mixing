2d5887122bc4825a89593ef0863dbf97db8485d2
Henri Drake <henrifdrake@gmail.com>
2022-12-30 09:04:40 -0500

    Change regime to subcritical and separate run! from plot!
    - Change aspect ratio of sinusoidal topography to be in slightly subcritical regime (ratio of maximum topographic slope to internal tide slope of about 0.8)
    - The set-up is almost an exact replica of Yi, Legg, and Nazarian (2017)'s MITgcm configuration, except that I have omitted the upper sponge layer for the time being and Lx -> Lx/5 and Lz -> Lz/2 to shorten run time. The main pattern of dissipation is unaffected, however.
    - The set-up is also very similar to Nikurashin and Legg (2011) except that their initial stratification increases upwards (representing a thermocline) and their use fuller topographic spectrum that better reflects abyssal hills morphology
    - I separated the simulation and plotting scrips for convenience of development
    - The plotting now uses CairoMakie (which is amazing!) instead of Plots.jl

---
+++ internal_tide_plots.jl
@@ -0,0 +1,78 @@
+using Printf
+using Oceananigans
+using Oceananigans.Units
+using Oceananigans.TurbulenceClosures: VerticallyImplicitTimeDiscretization
+using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBoundary
+using CairoMakie
+using JLD2
+
+function nice_divergent_levels(c, clim; nlevels=20)
+    levels = range(-clim, stop=clim, length=nlevels)
+    cmax = maximum(abs, c)
+    clim < cmax && (levels = vcat([-cmax], levels, [cmax]))
+    return (-clim, clim), levels
+end
+
+function nan_solid(x, z, u, mask)
+    Nx, Nz = size(u)
+    x2 = reshape(x, (Nx, 1))
+    z2 = reshape(z, (1, Nz))
+    u[mask] .= NaN
+    return u
+end
+
+saved_output_filename = "internal_tide_NL2010_2-day-test.jld2"
+file = jldopen(saved_output_filename)
+
+ig = file["serialized/grid"]
+ug = ig.underlying_grid
+
+u = FieldTimeSeries(saved_output_filename, "u")
+ε = FieldTimeSeries(saved_output_filename, "ε")
+b = FieldTimeSeries(saved_output_filename, "b")
+t = b.times
+
+xu, yu, zu = nodes(u[1])
+xb, yb, zb = nodes(b[1])
+
+# transform ε to logspace
+for n in 1:length(t)
+    ε[n].data .= log10.(ε[n].data)
+end
+
+n = Observable(1)
+
+# mask immersed boundaries
+mask = interior(ig.immersed_boundary.mask)[:,1,:]
+uₙ = @lift nan_solid(xu, zu, interior(u[$n], :, 1, :), mask)
+εₙ = @lift nan_solid(xb, zb, interior(ε[$n], :, 1, :), mask)
+bₙ = @lift nan_solid(xb, zb, interior(b[$n], :, 1, :), mask)
+
+begin
+    fig = Figure(resolution = (1000, 1000), figure_padding=(10, 40, 10, 10))
+    axis_kwargs = (xlabel = "zonal distance (x)",
+                      ylabel = "elevation (z)",
+                      limits = ((0, ug.Lx), (0, ug.Lz)),
+                      )
+    ax_u = Axis(fig[1, 1]; title = "TKE dissipation rate (ε) and equally-spaced buoyancy contours (b)", axis_kwargs...)
+    ax_ε = Axis(fig[2, 1]; title = "zonal velocity (u) and equally-spaced buoyancy contours (b)", axis_kwargs...)
+end
+
+U₀ = 0.025
+hm_u = heatmap!(ax_u, xu, zu, uₙ,
+    colorrange = (-3U₀, 3U₀), colormap = :balance,
+    lowclip=cgrad(:balance)[1], highclip=cgrad(:balance)[end])
+ct_u = contour!(ax_u, xb, zb, bₙ,
+    levels=0.:0.5e-4:3.e-3, linewidth=0.25, color=:black, alpha=0.2)
+
+hm_ε = heatmap!(ax_ε, xb, zb, εₙ,
+    colorrange = (-11, -8), colormap = :matter,
+    lowclip=cgrad(:matter)[1], highclip=cgrad(:matter)[end])
+ct_ε = contour!(ax_ε, xb, zb, bₙ,
+    levels=0.:0.5e-4:3.e-3, linewidth=0.25, color=:black, alpha=0.2)
+
+frames = (1:length(t))
+record(fig, "test.mp4", frames, framerate=16) do i
+    @info "Plotting frame $i of $(frames[end])..."
+    n[] = i
+end
--- internal_tide_replicate-NL2010.jl
+++ internal_tide_replicate-NL2010.jl
@@ -4,22 +4,24 @@ using Oceananigans.Units
 using Oceananigans.TurbulenceClosures: VerticallyImplicitTimeDiscretization
 using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBoundary
 using LinearAlgebra
-using Plots
 
-suffix = "10-day-test"
+suffix = "20-days"
 
 ## Simulation parameters
-Nx = 256
+Nx = 400
 Ny = 1
 Nz = 128
 
-tᶠ = 10days # simulation run time
+tᶠ = 20days # simulation run time
 Δtᵒ = 30minutes # interval for saving output
 
 H = 3kilometers # 6.e3 # vertical extent
-L = H # 60.e3 # horizontal extent
+L = 2*2H # 60.e3 # horizontal extent
 
-## Create grid 
+n = 2 # topographic wave number
+h = 200meters # topographic height
+
+## Create grid
 # Creates a vertical grid with near-constant spacing `refinement * Lz / Nz` near the bottom:
 # "Warped" coordinate
 kwarp(k, N) = (N + 1 - k) / N
@@ -39,12 +41,9 @@ grid = RectilinearGrid(size=(Nx, Ny, Nz),
         topology = (Periodic, Periodic, Bounded)
 )
 
-plot(grid.Δzᵃᵃᶜ[1:Nz], grid.zᵃᵃᶜ[1:Nz], xlim=(0, NaN))
-
 # Sinusoidal topography of height h and mode number n
-h = 200meters
-n = 3
-topog_mask(x, y, z) = z < h * (1 + cos(n*(2π*x/L + π)))/2 + 2minimum(grid.Δzᵃᵃᶜ)
+topog(x, y, z) = h * (1 + cos(n*2π*x/L + π))/2 + 2minimum(grid.Δzᵃᵃᶜ)
+topog_mask(x, y, z) = z < topog(x, y, z)
 
 # Create immersed boundary grid
 grid_with_bumps = ImmersedBoundaryGrid(grid, GridFittedBoundary(topog_mask))
@@ -57,6 +56,10 @@ f₀ = 0.53e-4
 U₀ = 0.025
 ω₀ = 1.4e-4
 tidal_forcing(x, y, z, t) = U₀*ω₀*cos(ω₀*t)
+
+s = sqrt((ω₀^2-f₀^2)/(N^2-ω₀^2))
+γ = h*π/(s*6kilometers)
+print("Wave steepness of γ=",round(γ, digits=3))
 
 model = NonhydrostaticModel(
     grid = grid_with_bumps,
@@ -90,82 +93,15 @@ simulation.output_writers[:fields] = JLD2OutputWriter(model, all_diags,
                                         filename = fname,
                                         overwrite_existing = true)
 ## Progress messages
-
 progress_message(s) = @info @sprintf("[%.2f%%], iteration: %d, time: %.3f, max|w|: %.2e",
                             100 * s.model.clock.time / s.stop_time, s.model.clock.iteration,
                             s.model.clock.time, maximum(abs, model.velocities.w))
 simulation.callbacks[:progress] = Callback(progress_message, TimeInterval(Δtᵒ))
 
+## Running the simulation!
 run!(simulation)
 
 @info """
     Simulation complete.
     Output: $(abspath(simulation.output_writers[:fields].filepath))
 """
-
-using JLD2
-using Plots
-
-function nice_divergent_levels(c, clim; nlevels=20)
-    levels = range(-clim, stop=clim, length=nlevels)
-    cmax = maximum(abs, c)
-    clim < cmax && (levels = vcat([-cmax], levels, [cmax]))
-    return (-clim, clim), levels
-end
-
-function nan_solid(x, z, u, topog)
-    Nx, Nz = size(u)
-    x2 = reshape(x, (Nx, 1))
-    z2 = reshape(z, (1, Nz))
-    u[topog.(x2, 0., z2)] .= NaN
-    return nothing
-end
-
-function visualize_internal_tide_simulation(prefix)
-
-    filename = prefix * ".jld2"
-    file = jldopen(filename)
-
-    grid = file["serialized/grid"]
-
-    xu, yu, zu = nodes((Face, Center, Center), grid)
-    xw, yw, zw = nodes((Center, Center, Face), grid)
-    xb, yb, zb = nodes((Center, Center, Center), grid)
-
-    b₀ = file["timeseries/b/0"][:, 1, :]
-
-    iterations = parse.(Int, keys(file["timeseries/t"]))    
-
-    anim = @animate for (i, iter) in enumerate(iterations)
-
-        @info "Plotting iteration $iter of $(iterations[end])..."
-
-        u = file["timeseries/u/$iter"][:, 1, :]
-        b = file["timeseries/b/$iter"][:, 1, :]
-        ε = file["timeseries/ε/$iter"][:, 1, :]
-        t = file["timeseries/t/$iter"]
-
-        b′ = b .- b₀
-
-        ulims, ulevels = nice_divergent_levels(u, 2*U₀)
-        blims, blevels = nice_divergent_levels(b′, N^2*grid.Lz/1000.)
-        
-        nan_solid(xu, zu, u, topog_mask)
-        nan_solid(xb, zb, ε, topog_mask)
-        nan_solid(xb, zb, b, topog_mask)
-
-        u_title = @sprintf("x velocity, t = %.2f", t)
-
-        u_plot = contourf(xu, zu, u'; title = u_title,                  color = :balance, linewidth = 0, levels = ulevels, clims = ulims)
-        ε_plot = contourf(xb, zb, log10.(ε)'; title = "dissipation",    color = :matter, linewidth = 0, clims = (-11., -7.))
-        b_plot = contourf(xb, zb, b′'; title = "buoyancy perturbation", color = :delta, linewidth = 0, levels = blevels, clims = blims)
-
-        plot(u_plot, b_plot, ε_plot, layout = (3, 1), xlims=(0, L), ylims=(0, H/2), size = (1200, 1600), dpi=200)
-    end
-
-    mp4(anim, prefix * ".mp4", fps = 20)
-
-    close(file)
-end
-
-visualize_internal_tide_simulation(string("internal_tide_NL2010_", suffix))
