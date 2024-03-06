d0a861b32bf8585dff00e3d60493a2c0ea96ac0d
Henri Drake <henrifdrake@gmail.com>
2024-03-06 13:04:22 -0800

    Updated plots

--- internal_tide_plots.jl
+++ internal_tide_plots.jl
@@ -5,6 +5,7 @@ using Oceananigans.TurbulenceClosures: VerticallyImplicitTimeDiscretization
 using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBoundary
 using CairoMakie
 using JLD2
+using Statistics
 
 function nice_divergent_levels(c, clim; nlevels=20)
     levels = range(-clim, stop=clim, length=nlevels)
@@ -21,34 +22,19 @@ function nan_solid(x, z, u, mask)
     return u
 end
 
-saved_output_filename = "internal_tide_tilted-test.jld2"
+saved_output_filename = "internal_tide_40days-theta=0.0.jld2"
 file = jldopen(saved_output_filename)
 
 ig = file["serialized/grid"]
 ug = ig.underlying_grid
 ĝ = file["serialized/buoyancy"].gravity_unit_vector
 
-û = FieldTimeSeries(saved_output_filename, "uhat")
-ŵ = FieldTimeSeries(saved_output_filename, "what")
 ε = FieldTimeSeries(saved_output_filename, "ε")
-B = FieldTimeSeries(saved_output_filename, "B")
-t = B.times
+ε̄ = log10.(mean(interior(ε, :, 1, :, :)[:,:,end-200:end], dims=(3))[:,:])
 
-xu, yu, zu = nodes(û[1])
-xw, yw, zw = nodes(ŵ[1])
-xb, yb, zb = nodes(B[1])
+t = ε.times
 
-for n in 1:length(t)
-    ε[n].data .= log10.(@. ifelse(ε[n].data > 0, ε[n].data, 1.e-100)) # transform ε to logspace
-end
-
-n = Observable(1)
-
-# mask immersed boundaries
-mask = interior(ig.immersed_boundary.mask)[:,1,:]
-ûₙ = @lift nan_solid(xu, zu, interior(û[$n], :, 1, :), mask)
-εₙ = @lift nan_solid(xb, zb, interior(ε[$n], :, 1, :), mask)
-Bₙ = @lift nan_solid(xb, zb, interior(B[$n], :, 1, :), mask)
+xb, yb, zb = nodes(ε[1])
 
 begin
     fig = Figure(resolution = (1000, 1000), figure_padding=(10, 40, 10, 10))
@@ -56,25 +42,21 @@ begin
                       ylabel = "elevation (z)",
                       limits = ((0, ug.Lx), (0, ug.Lz)),
                       )
-    ax_u = Axis(fig[1, 1]; title = "zonal velocity (u) and equally-spaced buoyancy contours (B)", axis_kwargs...)
-    ax_ε = Axis(fig[2, 1]; title = "TKE dissipation rate (ε) and equally-spaced buoyancy contours (B)", axis_kwargs...)
+    ax_u = Axis(fig[2, 1]; title = "zonal velocity (u) and equally-spaced buoyancy contours (B)", axis_kwargs...)
+    ax_ε = Axis(fig[3, 1]; title = "TKE dissipation rate (ε) and equally-spaced buoyancy contours (B)", axis_kwargs...)
 end
+fig[1, :] = Label(fig, "t = 1", fontsize=20, tellwidth=false)
 
-U₀ = 0.025
-hm_u = heatmap!(ax_u, xu, zu, ûₙ,
-    colorrange = (-3U₀, 3U₀), colormap = :balance,
-    lowclip=cgrad(:balance)[1], highclip=cgrad(:balance)[end])
-ct_u = contour!(ax_u, xb, zb, Bₙ,
-    levels=0.:0.5e-4:4.e-3, linewidth=0.25, color=:black, alpha=0.2)
-
-hm_ε = heatmap!(ax_ε, xb, zb, εₙ,
-    colorrange = (-11, -8), colormap = :matter,
+hm_ε = heatmap!(ax_ε, xb, zb, ε̄,
+    colorrange = (-11, -9), colormap = :matter,
     lowclip=cgrad(:matter)[1], highclip=cgrad(:matter)[end])
-ct_ε = contour!(ax_ε, xb, zb, Bₙ,
-    levels=0.:0.5e-4:4.e-3, linewidth=0.25, color=:black, alpha=0.2)
+Colorbar(fig[2,2], hm_ε)
+Colorbar(fig[3,2], hm_ε)
+current_figure()
 
 frames = (1:length(t))
-record(fig, string(split(saved_output_filename, ".")[1], ".mp4"), frames, framerate=16) do i
+filename = join(split(saved_output_filename, ".")[1:end-1], ".")
+record(fig, string(filename,".mp4"), frames, framerate=16) do i
     @info "Plotting frame $i of $(frames[end])..."
     n[] = i
 end
