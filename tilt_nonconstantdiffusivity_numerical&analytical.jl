bbde0bb7ef319f2862a82aa065237c8483a89f1b
Chih-Lun (Aaron) Liu <chihlunl@gmail.com>
2024-07-04 17:42:58 -0700

    theta=1 (increasing theta helps faster adjustment, and the result matches with the analytical solution)

--- tilt_nonconstantdiffusivity_numerical&analytical.jl
+++ tilt_nonconstantdiffusivity_numerical&analytical.jl
@@ -17,14 +17,14 @@ using GLMakie
 using NCDatasets
 # using Oceanostics.PotentialEnergyEquationTerms: PotentialEnergy
 
-suffix = "250days"
+suffix = "50days"
 
 ## Simulation parameters
  Nx = 4  #150 #250 500 1000
  Ny = 4 #300 #500 1000 2000
- Nz = 80 #250
+ Nz = 250 #250
 
- tᶠ = 250days # simulation run time
+ tᶠ = 50days # simulation run time
  Δtᵒ = 5hours#0.5days # interval for saving output
 
  H = 2kilometers
@@ -44,7 +44,7 @@ kwarp(k, N) = (N + 1 - k) / N
 # large refinement gets better resolution at the bottom but coarser at the surface
 # large stretching: resolution difference between the bottom and surface increases
 # z_faces(k) = - H * (ζ(k, Nz, 1.8) * Σ(k, Nz, 10) - 1)
-z_faces(k) = - H * (ζ(k, Nz, 1.3) * Σ(k, Nz, 10) - 1)
+z_faces(k) = - H * (ζ(k, Nz, 1.8) * Σ(k, Nz, 10) - 1)
 
 
 grid = RectilinearGrid(size=(Nx, Ny, Nz), 
@@ -65,8 +65,8 @@ grid_immerse = ImmersedBoundaryGrid(grid, GridFittedBottom(bottomimmerse))
 
 # Environmental parameters
 N = 1.3e-3              # Brunt-Väisälä buoyancy frequency        
-f₀ = -5.5e-4            # Coriolis frequency
-θ = 2e-1# 2e-3                # tilting of domain in (x,z) plane, in radians [for small slopes tan(θ)~θ]
+f₀ = -5.5e-5            # Coriolis frequency
+θ = 1#2e-1# 2e-3                # tilting of domain in (x,z) plane, in radians [for small slopes tan(θ)~θ]
 ĝ = (sin(θ), 0, cos(θ)) # vertical (gravity-oriented) unit vector in rotated coordinates
 κ₀ = 6e-3#5.2e-4             # Far-Field diffusivity
 κ₁ = 1.8e-3             # Bottom enhancement of diffusivity
@@ -149,7 +149,7 @@ u, v, w = model.velocities
 û = @at (Face, Center, Center) u*ĝ[3] - w*ĝ[1] # true zonal velocity
 Bz = @at (Center, Center, Center) ∂z(B)            
 
-fname = string("nonconstantdiffusivity", suffix,"-theta=",string(θ),"_Nx4_Ny4_smallf")
+fname = string("nonconstantdiffusivity", suffix,"-theta=",string(θ),"_Nx4_Ny4")
 
 
 simulation.output_writers[:checkpointer] = Checkpointer(
