7d3a0dda6364d71bd3586901dc34defc50632187
Chih-Lun Liu <cliu28@dt-login02.delta.ncsa.illinois.edu>
2024-07-05 19:12:22 -0500

    set constants

--- run_internal_tide.jl
+++ run_internal_tide.jl
@@ -6,10 +6,10 @@
 include("initialize_internal_tide.jl")
 
 const θ = 3.6e-3 #1.2e-3
-Nx = 500
-Ny = 1000
-Nz = 250
-U₀ = 0.025
+const Nx = 500
+const Ny = 1000
+const Nz = 250
+const U₀ = 0.025
 # closure = ScalarDiffusivity(; ν=1e-4, κ=1e-4),
 closure = SmagorinskyLilly()
 simulation = initialize_internal_tide(θ; Nx, Ny, Nz, U₀, closure)
