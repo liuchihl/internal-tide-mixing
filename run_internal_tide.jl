485cf4cc85c32986d20454a92d24acc659a544a3
Chih-Lun Liu <cliu28@dt-login03.delta.ncsa.illinois.edu>
2024-07-11 13:09:51 -0500

    add more keywords

--- run_internal_tide.jl
+++ run_internal_tide.jl
@@ -8,20 +8,24 @@ const Nx = 500
 const Ny = 1000
 const Nz = 250        
 const Δtᵒ = 30minutes # interval for saving output
-const tᶠ = 1days      # endtime of the simulation
+const tᶠ = 3days      # endtime of the simulation
 const θ = 3.6e-3      # slope angle
 const U₀ = 0.025      # tidal amplitude
 const N = 1.e-3       # Buoyancy frequency
+const f₀ = -0.53e-4   # Coriolis frequency
+threeD_snapshot_interval = 2Δtᵒ
 closure = SmagorinskyLilly()
 # 3 modes to choose: "spinup", "test", "analysis"
 output_mode = "test"
 output_writer = true
+clean = false         # cleanup and overwrite existing output (if pickup=true, clean=false, and vice versa)
 simulation = initialize_internal_tide(simname, Nx, Ny, Nz; 
-                                    Δtᵒ = Δtᵒ, tᶠ=tᶠ, θ=θ, U₀=U₀, N=N, 
+                                    Δtᵒ=Δtᵒ, tᶠ=tᶠ, θ=θ, U₀=U₀, N=N, f₀=f₀,
                                     output_mode=output_mode, output_writer=output_writer,
-                                    closure=closure)
+                                    threeD_snapshot_interval=threeD_snapshot_interval, 
+                                    clean=clean, closure=closure)
 ## Running the simulation!
-run!(simulation; pickup=false)
+run!(simulation; pickup=true)
 @info """
     Simulation complete.
 """
