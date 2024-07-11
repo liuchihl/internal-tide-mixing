090f239f913300cd36193cf37a051a9764a59a3d
Chih-Lun Liu <cliu28@dt-login04.delta.ncsa.illinois.edu>
2024-07-10 20:36:56 -0500

    update run file

--- run_internal_tide.jl
+++ run_internal_tide.jl
@@ -3,15 +3,23 @@
 
 include("initialize_internal_tide.jl")
 
-const simname = "reference"
+simname = "smag_tilt"
 const Nx = 500
 const Ny = 1000
-const Nz = 250
-const θ = 3.6e-3
-const U₀ = 0.025
+const Nz = 250        
+const Δtᵒ = 30minutes # interval for saving output
+const tᶠ = 1days      # endtime of the simulation
+const θ = 3.6e-3      # slope angle
+const U₀ = 0.025      # tidal amplitude
+const N = 1.e-3       # Buoyancy frequency
 closure = SmagorinskyLilly()
-simulation = initialize_internal_tide(simname, Nx, Ny, Nz; θ=θ, U₀=U₀, closure=closure)
-
+# 3 modes to choose: "spinup", "test", "analysis"
+output_mode = "test"
+output_writer = true
+simulation = initialize_internal_tide(simname, Nx, Ny, Nz; 
+                                    Δtᵒ = Δtᵒ, tᶠ=tᶠ, θ=θ, U₀=U₀, N=N, 
+                                    output_mode=output_mode, output_writer=output_writer,
+                                    closure=closure)
 ## Running the simulation!
 run!(simulation; pickup=false)
 @info """
