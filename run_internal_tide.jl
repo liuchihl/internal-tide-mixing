9e08db9295d8f8f8e9f19aec575d916339b60ce4
Chih-Lun Liu <cliu28@dt-login03.delta.ncsa.illinois.edu>
2024-07-12 14:28:35 -0500

    increase resolution at the top to see if reduction in stratification disappears

--- run_internal_tide.jl
+++ run_internal_tide.jl
@@ -3,14 +3,14 @@
 
 include("initialize_internal_tide.jl")
 
-simname = "test_memorylog"
+simname = "higher_top_resolution"
 const Nx = 500
 const Ny = 1000
 const Nz = 250        
 const Δtᵒ = 30minutes # interval for saving output
 const tᶠ = 3days      # endtime of the simulation
 const θ = 3.6e-3      # slope angle
-const U₀ = 0.      # tidal amplitude
+const U₀ = 0.025      # tidal amplitude
 const N = 1.e-3       # Buoyancy frequency
 const f₀ = -0.53e-4   # Coriolis frequency
 threeD_snapshot_interval = 2Δtᵒ
@@ -18,14 +18,14 @@ closure = SmagorinskyLilly()
 # 3 modes to choose: "spinup", "test", "analysis"
 output_mode = "test"
 output_writer = true
-clean = false         # cleanup and overwrite existing output (if pickup=true, clean=false, and vice versa)
+clean = true         # cleanup and overwrite existing output (if pickup=true, clean=false, and vice versa)
 simulation = initialize_internal_tide(simname, Nx, Ny, Nz; 
                                     Δtᵒ=Δtᵒ, tᶠ=tᶠ, θ=θ, U₀=U₀, N=N, f₀=f₀,
                                     output_mode=output_mode, output_writer=output_writer,
                                     threeD_snapshot_interval=threeD_snapshot_interval, 
                                     clean=clean, closure=closure)
 ## Running the simulation!
-run!(simulation; pickup=true)
+run!(simulation; pickup=false)
 @info """
     Simulation complete.
 """
