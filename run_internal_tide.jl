97d3af3c9391843e6fa42b02be6fe4c649331ea1
Chih-Lun Liu <cliu28@dt-login01.delta.ncsa.illinois.edu>
2024-07-14 12:14:03 -0500

    add timerange, add clean_checkpoint and overwrite_output.

--- run_internal_tide.jl
+++ run_internal_tide.jl
@@ -8,7 +8,7 @@ const Nx = 500
 const Ny = 1000
 const Nz = 250        
 const Δtᵒ = 30minutes # interval for saving output
-const tᶠ = 3days      # endtime of the simulation
+const tᶠ = 25days      # endtime of the simulation
 const θ = 3.6e-3      # slope angle
 const U₀ = 0.025      # tidal amplitude
 const N = 1.e-3       # Buoyancy frequency
@@ -18,14 +18,16 @@ closure = SmagorinskyLilly()
 # 3 modes to choose: "spinup", "test", "analysis"
 output_mode = "test"
 output_writer = true
-clean = true         # cleanup and overwrite existing output (if pickup=true, clean=false, and vice versa)
+clean_checkpoint = false         # cleanup checkpoint
+overwrite_output = true          # overwrite existing output (if pickup=true, clean=false, and vice versa)
+timerange = "5-25"
 simulation = initialize_internal_tide(simname, Nx, Ny, Nz; 
-                                    Δtᵒ=Δtᵒ, tᶠ=tᶠ, θ=θ, U₀=U₀, N=N, f₀=f₀,
-                                    output_mode=output_mode, output_writer=output_writer,
-                                    threeD_snapshot_interval=threeD_snapshot_interval, 
-                                    clean=clean, closure=closure)
+                                Δtᵒ=Δtᵒ, tᶠ=tᶠ, θ=θ, U₀=U₀, N=N, f₀=f₀,
+                                output_mode=output_mode, output_writer=output_writer,
+                                threeD_snapshot_interval=threeD_snapshot_interval, 
+                                clean_checkpoint=clean, overwrite_output=overwrite_output, closure=closure)
 ## Running the simulation!
-run!(simulation; pickup=false)
+run!(simulation; pickup=true)
 @info """
     Simulation complete.
 """
