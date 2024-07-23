ac99d3e39ed00dcc7002c820ef49df80ef382c9e
Chih-Lun Liu <cliu28@dt-login01.delta.ncsa.illinois.edu>
2024-07-23 00:11:10 -0500

    Change InterationInterval to TimeInterval (able to reproduce the issue during post-checkpoint runs)

--- oneD_diffusion_checkpoint_test.jl
+++ oneD_diffusion_checkpoint_test.jl
@@ -1,7 +1,8 @@
 using Oceananigans
+using Oceananigans.Units
 using Printf
 
-grid = RectilinearGrid(CPU(),size=128, z=(-0.5, 0.5), topology=(Flat, Flat, Bounded))
+grid = RectilinearGrid(CPU(),size=128, z=(0, 10meters), topology=(Flat, Flat, Bounded))
 
 closure = ScalarDiffusivity(κ=1)
 
@@ -15,10 +16,10 @@ set!(model, T=initial_temperature)
 ## Time-scale for diffusion across a grid cell
 min_Δz = minimum_zspacing(model.grid)
 diffusion_time_scale = min_Δz^2 / model.closure.κ.T
-iteration_final = 5000
+iteration_final = 5minutes
 simulation = Simulation(model, Δt = 0.1 * diffusion_time_scale, stop_iteration = iteration_final)
 
-dir = "test"
+dir = "test_MWE"
 ## checkpoint  
 simulation.output_writers[:checkpointer] = Checkpointer(
     model,
@@ -27,20 +28,20 @@ simulation.output_writers[:checkpointer] = Checkpointer(
     prefix=string("_checkpoint"),
     cleanup=false)
 
-simulation.output_writers[:temperature] = NetCDFOutputWriter(model, model.tracers,
-                                        verbose=true,
-                                        filename = string(dir,"/one_dimensional_diffusion.nc"),
-                                        overwrite_existing = false,
-                                        schedule = IterationInterval(100))
+# simulation.output_writers[:temperature] = NetCDFOutputWriter(model, model.tracers,
+#                                         verbose=true,
+#                                         filename = string(dir,"/one_dimensional_diffusion.nc"),
+#                                         overwrite_existing = false,
+#                                         schedule = TimeInterval(1000))
 
 progress_message(s) = @info @sprintf("iteration: %d, time: %.3f, max|w|: %.2e, Δt: %.3f,
                     advective CFL: %.2e, diffusive CFL: %.2e\n",
                     s.model.clock.iteration,
                     s.model.clock.time, maximum(abs, model.velocities.w), s.Δt,
                     AdvectiveCFL(s.Δt)(s.model), DiffusiveCFL(s.Δt)(s.model))
-simulation.callbacks[:progress] = Callback(progress_message, IterationInterval(100))
+simulation.callbacks[:progress] = Callback(progress_message, TimeInterval(0.01))
             
-file = "MWE/_checkpoint_iteration2000.jld2"
+file = "test_MWE/_checkpoint_iteration60.jld2"
 run!(simulation; pickup=file)
 
 
