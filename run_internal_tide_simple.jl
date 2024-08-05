eade014bfa4c5c1be9562333d171119f1416e34f
Chih-Lun Liu <cliu28@dt-login01.delta.ncsa.illinois.edu>
2024-08-05 17:40:28 -0500

    file for simple test runs

--- run_internal_tide_simple.jl
+++ run_internal_tide_simple.jl
@@ -3,12 +3,12 @@
 
 include("initialize_internal_tide.jl")
 
-simname = "test"
+simname = "tests"
 const Nx = 10
 const Ny = 4
 const Nz = 20        
 const Δtᵒ = 30minutes # interval for saving output
-const tᶠ = 1days      # endtime of the simulation
+const tᶠ = 4days      # endtime of the simulation
 const θ = 3.6e-3      # slope angle
 const U₀ = 0.025      # tidal amplitude
 const N = 1.e-3       # Buoyancy frequency
@@ -20,7 +20,7 @@ output_mode = "test"
 output_writer = true
 clean_checkpoint = false         # cleanup checkpoint
 overwrite_output = true          # overwrite existing output (if pickup=true, clean=false, and vice versa)
-timerange = "0-1"
+timerange = "3.5-4"
 simulation = initialize_internal_tide(simname, Nx, Ny, Nz; 
                                 Δtᵒ=Δtᵒ, tᶠ=tᶠ, θ=θ, U₀=U₀, N=N, f₀=f₀,
                                 output_mode=output_mode, output_writer=output_writer,
@@ -28,8 +28,8 @@ simulation = initialize_internal_tide(simname, Nx, Ny, Nz;
                                 clean_checkpoint=clean_checkpoint, overwrite_output=overwrite_output, 
                                 closure=closure, timerange=timerange)
 ## Running the simulation!
-# pickup = string("output/", simname, "/internal_tide_theta=0.0036_realtopo3D_Nx=10_Nz=20_2-2.5_checkpoint_iteration6256.jld2")
-run!(simulation; pickup=false)
+pickup = string("output/", simname, "/internal_tide_theta=0.0036_realtopo3D_Nx=10_Nz=20_3-3.5_checkpoint_iteration11156.jld2")
+run!(simulation; pickup=pickup)
 @info """
     Simulation complete.
 """
