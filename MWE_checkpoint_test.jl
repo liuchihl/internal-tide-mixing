8d79d5ab010a6da17e766ca26e41e4850cfcbd97
Chih-Lun Liu <cliu28@dt-login01.delta.ncsa.illinois.edu>
2024-08-06 19:09:26 -0500

    update

--- MWE_checkpoint_test.jl
+++ MWE_checkpoint_test.jl
@@ -2,16 +2,16 @@ using Oceananigans
 using Oceananigans.Units
 using Printf
 
-Ns = 200    # number of time saves
-T = 800 # simulation stop time (s)
+Ns = 10    # number of time saves
+T = 200 # simulation stop time (s)
 Δt = 1  # timestep (s)
 
-grid = RectilinearGrid(size=(), topology=(Flat, Flat, Flat))
+grid = RectilinearGrid(size=(), topology=(Oceananigans.Flat, Oceananigans.Flat, Oceananigans.Flat))
 model = NonhydrostaticModel(; grid, timestepper=:RungeKutta3)
 simulation = Simulation(model; Δt, stop_time = T)
 progress_message(sim) = @printf("Iteration: %03d, time: %s, Δt: %s, wall time: %s\n",
 	iteration(sim), prettytime(sim), prettytime(sim.Δt), prettytime(sim.run_wall_time))
-simulation.callbacks[:progress] = Callback(progress_message, TimeInterval(T/Ns))
+simulation.callbacks[:progress] = Callback(progress_message, TimeInterval(10))
 dir = "output/test_MWE"
 ## checkpoint  
 simulation.output_writers[:checkpointer] = Checkpointer(
@@ -21,6 +21,6 @@ simulation.output_writers[:checkpointer] = Checkpointer(
                             prefix=string("checkpoint"),
                             cleanup=true)
 # simulation.callbacks[:progress].schedule.actuations = 1
-file = string(dir,"/checkpoint_iteration400.jld2")
+file = string(dir,"/checkpoint_iteration100.jld2")
 
 run!(simulation,pickup=file)
