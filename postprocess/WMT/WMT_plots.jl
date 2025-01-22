cc4860b1d64193ec2139f2faeb9fa9ef4adf2200
Chih-Lun Liu <cliu28@dt-login01.delta.ncsa.illinois.edu>
2025-01-21 21:03:08 -0600

    move WMT folder to postprocess

---
+++ postprocess/WMT/WMT_plots.jl
@@ -0,0 +1,187 @@
+## this script plots water mass transformation results computed in water_mass_transformation.jl
+     using NCDatasets
+     # load data
+     slope = "tilt"
+     timerange = "100-120" 
+     θ=3.6e-3
+     ds_total = Dataset(string("output/",slope,"/WMT_120_added_diffusivity",".nc"),"r")
+     ∇κ∇B_t = ds_total["∇κ∇B_t"][:,:,1]
+     div_uB_t = ds_total["div_uB_t"][:,:,1]
+     
+     ds_canyon = Dataset(string("output/",slope,"/WMT_canyon_120",".nc"),"r")
+     ∇κ∇B_c = ds_canyon["∇κ∇B_t"][:,:,1]
+     div_uB_c = ds_canyon["div_uB_t"][:,:,1]
+     
+     ds_flank = Dataset(string("output/",slope,"/WMT_flanks_120",".nc"),"r")
+     ∇κ∇B_f = ds_flank["∇κ∇B_t"][:,:,1]
+     div_uB_f = ds_flank["div_uB_t"][:,:,1]
+     
+     hab = ds_total["bin_center1"][:]
+     b_bin = ds_total["bin_center2"][:]
+     Ly = 30e3
+     Lx = Ly/2
+     A = Lx*Ly
+## plot diffusive and advective terms together with respect to HAB with three different regions
+    using PyPlot 
+    using Statistics
+    close(gcf())
+    # Create a figure and an array of subplots
+    fig, axs = subplots(1, 2, figsize=(6, 3.5))  # 1 row, 2 columns
+
+    colors = [150 148 255;136 194 115;255 41 41]./255
+
+    # line2,=axs.plot(-dropdims(mean(div_uB_t,dims=2),dims=(2))*1e-3, hab, label="", color="red", linestyle="-",marker="",markersize=3)
+    line1,=axs[1].plot(dropdims(mean(∇κ∇B_t,dims=2),dims=(2))*1e-3, hab, label="", color=colors[1,:], marker=".",markersize=6)
+    
+    # line4,=axs.plot(-dropdims(mean(div_uB_c,dims=2),dims=(2))*1e-3*1/3, hab, label="", color="red", linestyle="--", marker="",markersize=3)
+    line3,=axs[1].plot(dropdims(mean(∇κ∇B_c,dims=2),dims=(2))*1e-3, hab, label="", color=colors[2,:], linestyle="--", marker=".",markersize=6)
+    
+    # line6,=axs.plot(-dropdims(mean(div_uB_f,dims=2),dims=(2))*1e-3./(2/3*A), hab, label="", color="red", linestyle=":", marker="",markersize=3)
+    line5,=axs[1].plot(dropdims(mean(∇κ∇B_f,dims=2),dims=(2))*1e-3, hab, label="", linestyle=":", color=colors[3,:], marker=".",markersize=6)
+    axs[1].set_ylabel("HAB (m)")
+    axs[1].set_xlabel("mSv")
+    # axs.legend([line1, line2], 
+    # [L"\frac{\partial}{\partial B}\iiint_{V(B'\leq B)} ∇⋅κ∇B ~dV", 
+    # L"-\frac{\partial}{\partial B}\iiint_{V(B'\leq B)} ∇⋅(\mathbf{u}B) ~dV"],
+    # loc="upper right")
+    axs[1].legend([line1, line3, line5], title=L"\mathcal{E}^{diff}",
+    ["Total","Canyon","Flanks"], loc="upper right")
+    axs[1].grid(true)
+    axs[1].set_ylim(0,70)
+    axs[1].minorticks_on()
+
+    # plot histogram
+    ∇κ∇B_t_BBL = mean(sum(∇κ∇B_t[1:3,:]*1e-3,dims=1))
+    ∇κ∇B_c_BBL = mean(sum(∇κ∇B_c[1:3,:]*1e-3,dims=1))
+    ∇κ∇B_f_BBL = mean(sum(∇κ∇B_f[1:3,:]*1e-3,dims=1))
+
+    data1 = [∇κ∇B_t_BBL,∇κ∇B_c_BBL,∇κ∇B_f_BBL]
+    data2 = [∇κ∇B_t_BBL,∇κ∇B_c_BBL/(1/3),∇κ∇B_f_BBL/(2/3)]
+    labels = ["Total","Canyon","Flanks"]
+    bar1=axs[2].bar(labels, data2, edgecolor = "black",color=colors,alpha=0.2)
+    bar2=axs[2].bar(labels, data1, edgecolor = "black",color=colors,alpha=1, width=0.5)
+    axs[2].set_title("Transport in BBL (mSv)")
+    axs[2].set_ylim(0,220)
+    axs[2].legend([bar1[1],bar2[1]],[L"\mathcal{E}_{BBL}^{diff}\times A_R/A",L"\mathcal{E}_{BBL}^{diff}"],loc="upper left")
+    
+
+    tight_layout()
+    gcf()   
+    savefig(string("output/",slope,"/water_mass_transformation_terms_120_together_differentregions",".png"),dpi=250)
+
+
+## plot diffusive and advective terms together with respect to HAB
+    using PyPlot 
+    using Statistics
+    close(gcf())
+    # Create a figure and an array of subplots
+    fig, axs = subplots(1, 1, figsize=(3.5, 3.5))  # 1 row, 2 columns
+
+    line1,=axs.plot(dropdims(mean(∇κ∇B_t,dims=2),dims=(2))*1e-3, hab, label="", color=[71,147,175]./255, marker=".",markersize=6)
+    line2,=axs.plot(dropdims(mean(div_uB_t,dims=2),dims=(2))*1e-3, hab, label="", color="red", marker=".",markersize=6)
+    axs.set_ylabel("HAB (m)")
+    axs.set_xlabel("mSv")
+    axs.legend([line1, line2], 
+    [L"\frac{\partial}{\partial B}\iiint_{V(B'\leq B)} ∇⋅κ∇B ~dV", 
+     L"\frac{\partial}{\partial B}\iiint_{V(B'\leq B)} ∇⋅(\mathbf{u}B) ~dV"],
+    loc="upper right")
+    axs.grid(true)
+    axs.set_ylim(0,200)
+    axs.set_xlim(-30,120)
+    minorticks_on()
+    tight_layout()
+    gcf()   
+    savefig(string("output/",slope,"/water_mass_transformation_terms_120_together_added_diffusivity",".png"),dpi=250)
+
+    
+## 
+    close(gcf())
+    fig, axs = subplots(1, 2, figsize=(8, 5))  # 1 row, 2 columns
+
+    cmap = PyPlot.get_cmap("coolwarm") 
+    pc1 = axs[1].pcolor(b_bin,hab,∇κ∇B_t,cmap=cmap)
+    pc1.set_clim(-1000,1000)
+    axs[1].set_ylim(0,250)
+    axs[1].set_xlabel("B")
+    axs[1].set_ylabel("HAB (m)")
+    axs[1].set_title(L"\frac{\partial}{\partial B}\iiint_V ∇⋅κ∇B ~dV")
+        # colorbar(pc1, ax=axs[1])  # Add colorbar to first subplot
+
+    cmap = PyPlot.get_cmap("coolwarm") 
+    pc2 = axs[2].pcolor(b_bin,hab,div_uB_t,cmap=cmap)
+    pc2.set_clim(-1500,1500)
+    axs[2].set_ylim(0,250)
+    colorbar(pc2, ax=axs[2])  # Add colorbar to first subplot
+    axs[2].set_xlabel("B")
+    axs[2].set_title(L"\frac{\partial}{\partial B}\iiint_V ∇⋅(\mathbf{u}B) ~dV")
+    gcf()
+    savefig(string("output/",slope,"/water_mass_transformation_terms_pcolor",".png"),dpi=100)
+    
+    
+    
+## plot the heatmap of ∇κ∇B 
+    using NCDatasets
+    slope = "tilt"
+    timerange = "100-120"
+    θ=3.6e-3
+    filename_3D = string("output/", slope, "/internal_tide_theta=",θ,"_realtopo3D_Nx=500_Nz=250_", timerange, "_threeD.nc")
+    ds_3D = Dataset(filename_3D,"r")
+    filename_field = string("output/", slope, "/internal_tide_theta=",θ,"_realtopo3D_Nx=500_Nz=250_", timerange, "_threeD_timeavg.nc")
+    ds_field = Dataset(filename_field,"r")
+   
+    # grids
+    zC = ds_field["zC"][:]; zF = ds_field["zF"][:];
+    Nz=length(zC[:]); 
+    xC = ds_field["xC"][:]; xF = ds_field["xF"][:]; 
+    Nx=length(xC[:]);       dx = xF[end]-xF[end-1];
+
+    yC = ds_field["yC"][:]; yF = ds_field["yF"][:]
+    Ny=length(yC[:]);       dy = yF[end]-yF[end-1];
+    t = ds_3D["time"][:];
+    n=40
+    j=1
+    ∇κ∇B = ds_3D["∇κ∇B"][:,j:j,:,n:n]; 
+    # b = ds_field["b"][:,j:j,:,n:n]; 
+    ∇κ∇B[∇κ∇B.==0].=NaN
+
+
+    using PyPlot
+    PyPlot.rc("font", size=14)  # Sets base font size
+    mpl_colors = PyPlot.pyimport("matplotlib.colors")
+    linthresh = 1.0
+    close(gcf())
+    norm_log = mpl_colors["SymLogNorm"](linthresh=linthresh, vmin=-1e-14, vmax=1e-14, base=10)
+    fig = figure(figsize=(13, 6))  # 10 inches wide by 6 inches tall
+ 
+    
+    # ax = subplots()
+    c = PyPlot.pcolor(xC./1000, zC, ∇κ∇B[:,1,:,1]',cmap="seismic",norm=norm_log)
+    cbar = PyPlot.colorbar(c, extend="both")
+    cbar.set_label("∇⋅(κ∇B)")
+    # tick_locations = [-1e-9, -1e-10, 0, 1e-10, 1e-9]
+    # Set the tick locations
+    # cbar.set_ticks(tick_locations)
+    # Set the background color of the axes to gray
+    ax = gca()
+    ax.set_facecolor("gray")
+
+
+    xlabel("x (km)")
+    ylabel("z (m)")
+    minorticks_on()
+    tight_layout()
+    PyPlot.draw()
+    gcf()
+
+    savefig("output/tilt/heatmap_∇κ∇B_test_constant_kappa.png",dpi=100)
+
+    using PyPlot
+    close(gcf)
+    fig, ax = subplots()  # Create a new figure and axes
+    ax.set_facecolor("gray")  # Set the axes background color
+    
+    # Plot something simple
+    ax.plot([0, 1], [0, 1])
+    gcf()
+    # Show the plot
+    show()
---
+++ postprocess/WMT/stitching_plot.jl
@@ -0,0 +1,165 @@
+# This script stitches the simulation together
+using CairoMakie
+using NCDatasets
+using Statistics
+using NaNStatistics
+using Interpolations
+##
+function deriv(z,y)
+   dydz =  diff(y[:,:,:,:],dims=3)./reshape(diff(z[:]),1,1,length(zC)-1)
+   return dydz
+end
+
+# load data
+    slope = "tilt"
+    timerange = "80-120"
+    θ=3.6e-3
+
+    filename_field = string("output/", slope, "/internal_tide_theta=",θ,"_realtopo3D_Nx=500_Nz=250_", timerange, "_threeD_timeavg.nc")
+    ds_field = Dataset(filename_field,"r")
+    filename_field_budget = string("output/", slope, "/internal_tide_theta=",θ,"_realtopo3D_Nx=500_Nz=250_", timerange, "_threeD_timeavg_Bbudget.nc")
+    ds_budget = Dataset(filename_field_budget,"r")
+    filename_3D = string("output/", slope, "/internal_tide_theta=",θ,"_realtopo3D_Nx=500_Nz=250_", timerange, "_threeD.nc")
+    ds_3D = Dataset(filename_3D,"r")
+  
+    # grids
+    zC = ds_3D["zC"][:]; zF = ds_3D["zF"][:];
+    Nz=length(zC[:]); 
+    xC = ds_3D["xC"][:]; xF = ds_3D["xF"][:]; 
+    Nx=length(xC[:]);       dx = xF[end]-xF[end-1];
+
+    yC = ds_3D["yC"][:]; yF = ds_3D["yF"][:]
+    Ny=length(yC[:]);       dy = yF[end]-yF[end-1];
+
+    t = ds_3D["time"][:];
+
+
+    θ = 3.6e-3
+# for n in 
+    n = 30
+    b = ds_field["b"][:,1,:,n:n];          # buoyancy perturbation
+    B = nanmean(ds_field["B"][:,1,:,n:n+10],dims=3);          # total buoyancy
+    # B = ds_field["B"][:,500,:,n:n];          # total buoyancy
+    # ∇κ∇B = ds_budget["∇κ∇B"][:,500,:,n:n];    # ∇⋅κ∇B: buoyancy flux divergence
+    ∇κ∇B = nanmean(ds_budget["∇κ∇B"][:,1,:,n:n+10],dims=3);    # ∇⋅κ∇B: buoyancy flux divergence
+    # div_uB = nanmean(ds_budget["div_uB"][:,500,:,30:40],dims=3);   
+    ∇κ∇B[b.==0] .= NaN;
+    # div_uB[b.==0] .= NaN;
+    B[b.==0] .= NaN;
+
+## coordinate transformation from slope-coordinate to Cartesian coordinate
+    Lx = (xF[end]+dx) * cos(θ)
+    Lz = (xF[end]+dx) * sin(θ)
+    x = xC * cos(θ) .- zC' * sin(θ)
+    z = xC * sin(θ) .+ zC' * cos(θ)
+    z_face = xF * sin(θ) .+ zF' * cos(θ)
+    y = yC
+    # extended coordinate
+    x̃ = x; z̃ = z; z̃_face = z_face; B̃ = B;
+    N = 1e-3
+    ΔB = N^2*Lz 
+
+    range = -1:-1:-40
+    for n in range
+    x̃ = [x .+ n*Lx ; x̃]
+    z̃ = [z .+ n*Lz ; z̃]
+    z̃_face = [z_face .+ n*Lz ; z̃_face]
+    B̃ = [B .+ n*ΔB ; B̃]
+    end
+    
+    num_repeats = length(range)+1
+
+    # Create the array of repeated matrices
+    ∇κ∇B̃ = (∇κ∇B for _ in 1:num_repeats)
+    ∇κ∇B̃ = vcat(∇κ∇B̃...)
+##
+    using PyPlot
+    PyPlot.rc("font", size=14)  # Sets base font size
+    mpl_colors = PyPlot.pyimport("matplotlib.colors")
+    linthresh = 1e-11
+    
+    norm_log = mpl_colors["SymLogNorm"](linthresh=linthresh, vmin=-1e-9, vmax=1e-9, base=10)
+    fig = figure(figsize=(20, 4))  # 10 inches wide by 6 inches tall
+    # ax = subplots()
+    c = PyPlot.pcolor(x̃./1000, z̃, ∇κ∇B̃[:,:,1],cmap="RdYlBu_r",norm=norm_log)
+    cbar = colorbar(c, extend="both")
+    cbar.set_label("∇⋅(κ∇B)")
+
+    # PyPlot.contour(x̃./1000, z̃, B̃[:,:,1],colors="black",(0.05:0.1:3).*1e-3,linewidths=1)
+    PyPlot.contour(x̃./1000, z̃, B̃[:,:,1],colors="black",(0.1:0.8:0.9).*1e-3,linewidths=2.3)
+    for n in -19:-1:-20
+    # PyPlot.plot([Lx*n,Lx*n]./1000, [-150,3000],linewidth=1.1,linestyle="-",color="green")
+    PyPlot.plot([Lx*n,Lx*n]./1000, [-1500,2300],linewidth=1.5,linestyle="-",color="green")
+    @show n, Lx*n./1000
+    end
+
+    ax = gca()
+    # Set the background color of the axes to gray
+    ax.set_facecolor("gray")
+    
+    xlabel("x (km)")
+    ylabel("z (m)")
+    ylim(-1000,2300)
+    minorticks_on()
+    tight_layout()
+
+    savefig(string("output/",slope,"/xz_buoyancy_extend_",timerange,"_y=0_extend40_t=120.png"),dpi=100)
+
+    # fig, axes = subplots(1,1, figsize=(10, 8))
+    # close(gcf())
+    # PyPlot.pcolor(xC[:],zC[:],B[:,1,:,1]')
+    # gcf()
+    # savefig(string("output/",slope,"/xz_buoyancy_extend_",timerange,".png"))
+
+
+
+
+## MWT
+
+
+    slope = "tilt"
+    timerange = "40-80"
+    θ=3.6e-3
+
+    filename_field = string("output/", slope, "/internal_tide_theta=",θ,"_realtopo3D_Nx=500_Nz=250_", timerange, "_threeD_timeavg.nc")
+    ds_field = Dataset(filename_field,"r")
+    filename_field_budget = string("output/", slope, "/internal_tide_theta=",θ,"_realtopo3D_Nx=500_Nz=250_", timerange, "_threeD_timeavg_Bbudget.nc")
+    ds_budget = Dataset(filename_field_budget,"r")
+
+    # grids
+    zC = ds_field["zC"][:]; zF = ds_field["zF"][:];
+    Nz=length(zC[:]); 
+    xC = ds_field["xC"][:]; xF = ds_field["xF"][:]; 
+    Nx=length(xC[:]);       dx = xF[end]-xF[end-1];
+
+    yC = ds_field["yC"][:]; yF = ds_field["yF"][:]
+    Ny=length(yC[:]);       dy = yF[end]-yF[end-1];
+
+    t = ds_field["time"][:];
+
+
+    θ = 3.6e-3
+    # for n in 
+    n = 40
+    b = ds_field["b"][:,500,:,n:n];          # buoyancy perturbation
+    B = nanmean(ds_field["B"][:,500,:,30:40],dims=3);          # total buoyancy
+    ∇κ∇B = nanmean(ds_budget["∇κ∇B"][:,500,:,30:40],dims=3);    # ∇⋅κ∇B: buoyancy flux divergence
+    div_uB = nanmean(ds_budget["div_uB"][:,500,:,30:40],dims=3);   
+    ∇κ∇B[b.==0] .= NaN;
+    div_uB[b.==0] .= NaN;
+    B[b.==0] .= NaN;
+
+
+
+
+
+
+    bin_edge = (1:2:40)*1e-4  # Define the edges of the bins
+
+    int_∇κ∇B̃,bin_center = bins(∇κ∇B̃[:,:,1],bin_edge,B̃,dx=dx,dy=dy,z_face=z̃_face,normalize=false)
+    using PyPlot
+    close(gcf())
+    plot(collect(bin_center),int_∇κ∇B̃)
+    gcf()
+
+    
