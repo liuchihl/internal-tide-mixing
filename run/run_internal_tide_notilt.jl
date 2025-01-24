8b605acc73b019439bc591e906ccc3ffcdb74781
Chih-Lun Liu <cliu28@dt-login03.delta.ncsa.illinois.edu>
2025-01-24 17:35:29 -0600

    add tidal freq

--- run/run_internal_tide_notilt.jl
+++ run/run_internal_tide_notilt.jl
@@ -2,6 +2,7 @@ include("internal_tide_setup.jl")
 # Parameters modified at run time
 const tp_end = 10
 # Derived parameters
+const ω₀ = 1.4e-4     # tidal freq.
 const tᶠ  = tp_end*2π/ω₀
 const θ = 0      # slope angle
 run_internal_tide(tᶠ,θ)
