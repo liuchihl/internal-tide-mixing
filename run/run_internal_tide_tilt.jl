37490d84289d8a8779de762ae2fac729bd4153e2
Chih-Lun Liu <cliu28@dt-login03.delta.ncsa.illinois.edu>
2025-01-27 15:09:42 -0600

    tilt simulation runs from 10-50TP

--- run/run_internal_tide_tilt.jl
+++ run/run_internal_tide_tilt.jl
@@ -1,6 +1,6 @@
 include("internal_tide_setup.jl")
 # Parameters modified at run time
-const tp_end = 10
+const tp_end = 50
 # Derived parameters
 const ω₀ = 1.4e-4     # tidal freq.
 const tᶠ  = tp_end*2π/ω₀
