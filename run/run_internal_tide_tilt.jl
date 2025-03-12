c1db801d2620b9df923ec6fec9dbbf290a760426
Chih-Lun Liu <cliu28@dt-login03.delta.ncsa.illinois.edu>
2025-03-12 14:01:23 -0500

    update

--- run/run_internal_tide_tilt.jl
+++ run/run_internal_tide_tilt.jl
@@ -1,6 +1,6 @@
 include("internal_tide_setup.jl")
 # Parameters modified at run time
-const tp_end =450
+const tp_end =460
 # Derived parameters
 const ω₀ = 1.4e-4     # tidal freq.
 const tᶠ  = tp_end*2π/ω₀
