0f2d071cafa5fd5742db12457d0abf28135a7329
Chih-Lun Liu <chihlunl@gmail.com>
2025-04-21 19:02:26 -0500

    Making an animation of hab integral of w and u and hab average Bz.

--- functions/interpolation_z_dimension.jl
+++ functions/interpolation_z_dimension.jl
@@ -1,56 +1,74 @@
 using Interpolations
 using NCDatasets
 using NaNStatistics
+
 @views function interpolate_z_dimension(var, hab, new_bin_edges)
-    
     """
-    Interpolate the z-dimension of a 4D matrix onto new bin edges.
+    Interpolate the z-dimension of a 3D or 4D matrix onto new bin edges.
     
     Parameters:
     -----------
-    var : Array{T,4}
-        Input 4D matrix (x, y, z, t)
-    hab : Array{T,3}
-        Height above bottom coordinate 
+    var : Array{T,3} or Array{T,4}
+        Input 3D (x, y, z) or 4D matrix (x, y, z, t)
+    hab : Array{T,3} or Array{T,2}
+        Height above bottom coordinate
     new_bin_edges : Vector{Float64}
         New z coordinates for interpolation
     
     Returns:
     --------
-    interpolated_var : Array{T,4}
-        Interpolated 4D matrix with new z dimension
+    interpolated_var : Array{T,3} or Array{T,4}
+        Interpolated matrix with new z dimension
     bin_centers : Vector{Float64}
         Centers of the new z bins
     """
-    # Get dimensions
-    nx, ny, nz, nt = size(var)
+    # Get dimensions and check dimensionality
+    dims = size(var)
+    is_4d = length(dims) == 4
+    
+    if is_4d
+        nx, ny, nz, nt = dims
+    else
+        nx, ny, nz = dims
+        nt = 1
+    end
     
     # Calculate new bin centers
     bin_centers = @. (new_bin_edges[1:end-1] + new_bin_edges[2:end]) / 2
     num_new_bins = length(bin_centers)
     
-    # Initialize output array
-    interpolated_var = zeros(nx, ny, num_new_bins, nt)
+    # Initialize output array with appropriate dimensions
+    if is_4d
+        interpolated_var = zeros(nx, ny, num_new_bins, nt)
+    else
+        interpolated_var = zeros(nx, ny, num_new_bins)
+    end
     
     # Interpolate for each x, y, and t
     for l in 1:nt
         for j in 1:ny
             for i in 1:nx
                 # Extract 1D slice for this x,y,t location
-                slice = var[i, j, :, l]
+                if is_4d
+                    slice = var[i, j, :, l]
+                    hab_slice = hab[i, j, :]
+                else
+                    slice = var[i, j, :]
+                    hab_slice = hab[i, j, :]
+                end
                 
                 # Create interpolation object
-                itp = LinearInterpolation(hab[i,j,:], slice, extrapolation_bc=Interpolations.Flat())
+                itp = LinearInterpolation(hab_slice, slice, extrapolation_bc=Interpolations.Flat())
                 
                 # Interpolate to new bin centers
-                interpolated_var[i, j, :, l] = itp(bin_centers)
+                if is_4d
+                    interpolated_var[i, j, :, l] = itp(bin_centers)
+                else
+                    interpolated_var[i, j, :] = itp(bin_centers)
+                end
             end
         end
     end
     
     return interpolated_var, bin_centers
 end
-
-
-
-
--- postprocess/terrain_following_slices.jl
+++ postprocess/terrain_following_slices.jl
@@ -3,11 +3,13 @@ function deriv(z,y)
     dydz =  diff(y[:,:,:,:],dims=3)./reshape(diff(z[:]),1,1,length(zC)-1)
     return dydz
  end
-
+include("functions/interpolation_z_dimension.jl")
 # test
 using Interpolations
-slope = "tilt"
-timerange = "80-120"
+using NCDatasets
+simname = "flat"
+# timerange = "80-120"
+tᶠ = 460 
 θ=3.6e-3
 # load data
 
@@ -15,8 +17,16 @@ filename_hab = "output/hab.nc"
 ds_hab = Dataset(filename_hab,"r")
 hab = ds_hab["hab"][:,:,:];
 
-filename_field = string("output/", slope, "/internal_tide_theta=",θ,"_realtopo3D_Nx=500_Nz=250_", timerange, "_threeD_timeavg.nc")
+filename_field = string("output/", simname, "/internal_tide_theta=",θ,"_Nx=500_Nz=250_tᶠ=",tᶠ, "_threeD_timeavg_const_dt_Bbudget-wb-eps-chi.nc")
 ds_field = Dataset(filename_field,"r")
+filename_velocity = string("output/", simname, "/internal_tide_theta=",θ,"_Nx=500_Nz=250_tᶠ=",tᶠ, "_threeD_timeavg_const_dt_u-v-w-Rig.nc")
+ds_velocity = Dataset(filename_velocity,"r")
+filename_B = string("output/", simname, "/internal_tide_theta=",θ,"_Nx=500_Nz=250_tᶠ=",tᶠ, "_threeD_timeavg_B-c.nc")
+ds_B = Dataset(filename_B,"r")
+filename_verification = string("output/", simname, "/internal_tide_theta=",θ,"_Nx=500_Nz=250_tᶠ=",10, "_threeD_timeavg.nc")   
+ds_verification = Dataset(filename_verification,"r")
+
+
 zC = ds_field["zC"][:]; zF = ds_field["zF"][:];
 Nz=length(zC[:]); 
 
@@ -27,45 +37,41 @@ yC = ds_field["yC"][:]; yF = ds_field["yF"][:]
 Ny=length(yC[:]);       dy = yF[end]-yF[end-1];
 t = ds_field["time"][:];
 
-new_bin_edges = 0:5:1500
+new_bin_edges = 0:7:1500
 bin_centers = @. (new_bin_edges[1:end-1] + new_bin_edges[2:end]) / 2
-interpolated_what = zeros(Nx,Ny,size(bin_centers,1),11)
-interpolated_uhat = zeros(Nx,Ny,size(bin_centers,1),11)
-interpolated_Bz = zeros(Nx,Ny,size(bin_centers,1),11)
-b = ds_field["b"][:,:,:,1:1];    # true w
-for n in 30:40
-    # uhat = ds_field["uhat"][:,:,:,n:n];    # true u
-    # what = ds_field["what"][:,:,:,n:n];    # true w
-    B = ds_field["B"][:,:,:,n:n];    # true w
-    
-    N = 1.e-3
-        Bz =  deriv(zC,B);
-        Bz[b[:,:,1:end-1,:].==0] .= 0      # the grids are staggered, but this will effectively set the points inside and right above the immersed boudary to 0
-    # interpolate Bz from faces to center cell
-    # Interpolate each row
-    Bz_center = zeros(size(Bz,1),size(Bz,2),length(zC),1)
-    for i in 1:size(Bz,1)
-        for j in 1:size(Bz,2)
-              itp = linear_interpolation(zF[2:end-1], Bz[i,j,:,1], extrapolation_bc=Line())
-            Bz_center[i,j,:,:] = itp(zC)
-        end
-    end
-    # Interpolate each row
-    # what_center = zeros(size(what,1),size(what,2),length(zC),1)
-    # for i in 1:size(what,1)
-    #     for j in 1:size(what,2)
-    #         itp = linear_interpolation(zF[:], what[i,j,:,1], extrapolation_bc=Line())
-    #         what_center[i,j,:,:] = itp(zC)
-    #     end
-    # end
+interpolated_what = zeros(Nx,Ny,size(bin_centers,1))
+interpolated_uhat = zeros(Nx,Ny,size(bin_centers,1))
+interpolated_Bz = zeros(Nx,Ny,size(bin_centers,1))
+uhat = nanmean(ds_velocity["uhat"][:,:,:,:],dim=4);    # true u
+what = nanmean(ds_velocity["what"][:,:,:,:],dim=4);    # true w
+B = nanmean(ds_B["B"][:,:,:,n:n],dim=4);    # true w
+b = ds_verification["b"][:,:,:,1:1];          # buoyancy perturbation
 
-    # @time interpolated_what[:,:,:,n-29],_ =  interpolate_z_dimension(what_center, hab, new_bin_edges)
-    # @time interpolated_uhat[:,:,:,n-29],_ =  interpolate_z_dimension(uhat, hab, new_bin_edges)
-    @time interpolated_Bz[:,:,:,n-29],_ =  interpolate_z_dimension(Bz_center, hab, new_bin_edges)
+N = 1.e-3
+Bz =  deriv(zC,B);
+Bz[b[:,:,1:end-1,1:1].==0] .= 0      # the grids are staggered, but this will effectively set the points inside and right above the immersed boudary to 0
+# interpolate Bz from faces to center cell
+# Interpolate each row
+Bz_center = zeros(size(Bz,1),size(Bz,2),length(zC))
+for i in 1:size(Bz,1)
+    for j in 1:size(Bz,2)
+            itp = linear_interpolation(zF[2:end-1], Bz[i,j,:], extrapolation_bc=Line())
+        Bz_center[i,j,:] = itp(zC)
+    end
 end
-# interpolated_what_tavg = nanmean(interpolated_what,dim =(4))
-# interpolated_uhat_tavg = nanmean(interpolated_uhat,dim =(4))
-interpolated_Bz_tavg = nanmean(interpolated_Bz,dim =(4))
+# Interpolate each row
+what_center = zeros(size(what,1),size(what,2),length(zC))
+for i in 1:size(what,1)
+    for j in 1:size(what,2)
+        itp = linear_interpolation(zF[:], what[i,j,:], extrapolation_bc=Line())
+        what_center[i,j,:] = itp(zC)
+    end
+end
+
+@time interpolated_what[:,:,:],_ =  interpolate_z_dimension(what_center, hab, new_bin_edges)
+@time interpolated_uhat[:,:,:],_ =  interpolate_z_dimension(uhat, hab, new_bin_edges)
+@time interpolated_Bz[:,:,:],_ =  interpolate_z_dimension(Bz_center, hab, new_bin_edges)
+
 # bathymetry
 using MAT
 file = matopen("topo.mat")
@@ -90,57 +96,200 @@ z_interp = [itp(x_topo_lin, y_topo_lin) for x_topo_lin in x_interp, y_topo_lin i
 z_interp = z_interp.-minimum(z_interp)
 
 
-using PyPlot
-close(gcf())
-PyPlot.plot(nanmean(interpolated_what_tavg[:,:,:],dim=(1,2)),bin_centers)
-gcf()
-savefig("output/tilt/interpolated_what_ln.png")
-
 
 using PyPlot
 
-close(gcf())
-fig, (ax1, ax2) = subplots(1, 2, figsize=(15, 5))
+bin_size = 7
+z_position = 7:7:49
+for i in 1:length(z_position)
+z_upper = argmin(abs.(bin_centers.-z_position[i]))
+end
 
-c1 = ax1.pcolor(xC[:]*1e-3, yC[:]*1e-3, nanmean(interpolated_what_tavg[:,:,1:19],dim=(3))',
+close(gcf())
+fig, (ax1, ax2, ax3) = subplots(1, 3, figsize=(15, 5))
+
+c1 = ax1.pcolor(xC[:]*1e-3, yC[:]*1e-3, nansum(interpolated_what[:,:,1:z_upper]*bin_size,dim=(3))',
                 cmap="RdYlBu_r",
-                vmin=-0.01, vmax=0.01)
+                vmin=-maximum(nansum(interpolated_what[:,:,1:z_upper]*bin_size,dim=(3))), vmax=maximum(nansum(interpolated_what[:,:,1:z_upper]*bin_size,dim=(3))))
 colorbar(c1, ax=ax1, extend="both")
-ax1.set_title(L"\frac{1}{95}∫^{95}_{0} \bar{w} dHAB")
+ax1.set_title("\$\\int_{0}^{" * string(round(Int, bin_centers[z_upper])) * "} \\bar{w} \\, dHAB\$")
 h1=ax1.contour(xC[:]*1e-3, yC[:]*1e-3, z_interp', linewidths=1.5, colors="black")
 PyPlot.clabel(h1, inline=true, fontsize=8)  # Add labels to contour lines
-ax1.set_xlabel("x (km)")
-ax1.set_ylabel("y (km)")
+ax1.set_xlabel("x [km]")
+ax1.set_ylabel("y [km]")
 
-c2 = ax2.pcolor(xC[:]*1e-3, yC[:]*1e-3, nanmean(interpolated_uhat_tavg[:,:,1:19],dim=(3))',
+c2 = ax2.pcolor(xC[:]*1e-3, yC[:]*1e-3, nanmean(interpolated_uhat[:,:,1:z_upper]*bin_size,dim=(3))',
                 cmap="RdYlBu_r",
-                vmin=-0.04, vmax=0.04)
+                vmin=-maximum(nanmean(interpolated_uhat[:,:,1:z_upper]*bin_size,dim=(3))), vmax=maximum(nanmean(interpolated_uhat[:,:,1:z_upper]*bin_size,dim=(3))))
 colorbar(c2, ax=ax2, extend="both")
-ax2.set_title(L"\frac{1}{95}∫^{95}_{0} \bar{u} dHAB")
+ax2.set_title("\$\\int_{0}^{" * string(round(Int, bin_centers[z_upper])) * "} \\bar{u} \\, dHAB\$")
 h2=ax2.contour(xC[:]*1e-3, yC[:]*1e-3, z_interp', linewidths=1.5, colors="black")
 PyPlot.clabel(h2, inline=true, fontsize=8)  # Add labels to contour lines
-ax2.set_xlabel("x (km)")
-ax2.set_ylabel("y (km)")
+ax2.set_xlabel("x [km]")
+ax2.set_ylabel("y [km]")
 
-savefig("output/tilt/interpolated_u_w_heatmap.png")
-
-
-# plot only Bz 
-using PyPlot
-
-close(gcf())
-fig, ax = subplots(1, 1, figsize=(5, 5))
-    # mpl_colors = PyPlot.pyimport("matplotlib.colors")
-    # linthresh = 1e-8
-    
-    # norm_log = mpl_colors["SymLogNorm"](linthresh=linthresh, vmin=-1e-7, vmax=1e-5, base=10)
-c = ax.pcolor(xC[:]*1e-3, yC[:]*1e-3, (nanmean(interpolated_Bz_tavg[:,:,1:19],dim=(3))'),
-                cmap="RdYlBu_r",
+c3 = ax3.pcolor(xC[:]*1e-3, yC[:]*1e-3, (nanmean(interpolated_Bz[:,:,1:z_upper],dim=(3))'),
+                cmap="GnBu",
                 vmin=0, vmax=2e-6)
-colorbar(c, ax=ax, extend="both")
-ax.set_title(L"\bar{B}_z (0-92.5 m)")
-h=ax.contour(xC[:]*1e-3, yC[:]*1e-3, z_interp', linewidths=1.5, colors="black")
-PyPlot.clabel(h, inline=true, fontsize=8)  # Add labels to contour lines
-ax.set_xlabel("x (km)")
-ax.set_ylabel("y (km)")
-savefig("output/tilt/interpolated_Bz_heatmap_0-92.5.png")
+colorbar(c3, ax=ax3, extend="both")
+ax3.set_title(L"\bar{B}_z" * string("[0-", round(Int,bin_centers[z_upper]), "m]"))
+h3=ax3.contour(xC[:]*1e-3, yC[:]*1e-3, z_interp', linewidths=1.5, colors="black")
+PyPlot.clabel(h3, inline=true, fontsize=8)  # Add labels to contour lines
+ax3.set_xlabel("x [km]")
+ax3.set_ylabel("y [km]")
+
+
+savefig(string("output/",simname,"/hab_u_w_Bz_xy_heatmap.png"))
+
+
+
+
+
+
+###### animation: 
+using CairoMakie
+using Printf
+using Statistics
+
+# Setting up the animation
+bin_size = 7
+z_position = 7:7:500
+
+# Create the output directory if it doesn't exist
+output_dir = string("output/", simname)
+isdir(output_dir) || mkpath(output_dir)
+
+# Full animation filename
+anim_filename = string(output_dir, "/hab_u_w_Bz_xy_animation.mp4")
+
+# Create Observable for the z-index
+n = Observable(1)
+
+# Create lift expressions for the data
+what_slice = @lift(nansum(interpolated_what[:,:,1:argmin(abs.(bin_centers .- z_position[$n]))]*bin_size, dims=3))
+uhat_slice = @lift(nanmean(interpolated_uhat[:,:,1:argmin(abs.(bin_centers .- z_position[$n]))]*bin_size, dims=3))
+Bz_slice = @lift(nanmean(interpolated_Bz[:,:,1:argmin(abs.(bin_centers .- z_position[$n]))], dims=3))
+
+# Create the figure
+# Create the figure with slightly more horizontal space to accommodate colorbars
+# Create the figure with more horizontal space to accommodate colorbars
+# Create the figure with a resolution that better fits the content
+# Create the figure with a resolution that fits the content
+fig = CairoMakie.Figure(resolution=(1200, 600), fontsize=16)
+
+# Create a custom grid layout with explicit column widths
+layout = GridLayout()
+
+# Assign the layout to the figure
+fig[1:2, 1] = layout
+
+# Create title with changing z value
+title = @lift("z = $(round(Int, bin_centers[argmin(abs.(bin_centers .- z_position[$n]))])) m")
+layout[1, 1:6] = Label(fig, title, fontsize=24)
+
+# Create axes with specific aspect ratios
+# Subplots will be in columns 1, 3, and 5; colorbars in columns 2, 4, and 6
+ax1 = Axis(layout[2, 1], 
+           title = L"\int_0^z \bar{w} \, dHAB",
+           xlabel = "x [km]", 
+           ylabel = "y [km]",
+           titlesize = 22)
+
+ax2 = Axis(layout[2, 3], 
+           title = L"\int_0^z \bar{u} \, dHAB",
+           xlabel = "x [km]",
+           titlesize = 22)
+
+ax3 = Axis(layout[2, 5], 
+           title = L"\bar{B}_z (0-z)",
+           xlabel = "x [km]",
+           titlesize = 22)
+
+# Set consistent aspect ratios
+ax1.aspect = 0.5
+ax2.aspect = 0.5
+ax3.aspect = 0.5
+
+# First plot: Vertical velocity
+hm1 = heatmap!(ax1, xC[:]*1e-3, yC[:]*1e-3, @lift(dropdims($what_slice, dims=3)),
+               colormap = reverse(cgrad(:RdYlBu)),
+               colorrange = @lift((-maximum(abs.(dropdims($what_slice, dims=3)))*0.5, 
+                                   maximum(abs.(dropdims($what_slice, dims=3)))*0.5)))
+cb1 = Colorbar(layout[2, 2], hm1, width=8)  # Place in column 2, slim width
+contour!(ax1, xC[:]*1e-3, yC[:]*1e-3, z_interp, linewidth=1.5, color=:black, levels=10)
+
+# Second plot: Horizontal velocity
+hm2 = heatmap!(ax2, xC[:]*1e-3, yC[:]*1e-3, @lift(dropdims($uhat_slice, dims=3)),
+               colormap = reverse(cgrad(:RdYlBu)),
+               colorrange = @lift((-maximum(abs.(dropdims($uhat_slice, dims=3)))*0.8, 
+                                   maximum(abs.(dropdims($uhat_slice, dims=3)))*0.8)))
+cb2 = Colorbar(layout[2, 4], hm2, width=8)  # Place in column 4, slim width
+contour!(ax2, xC[:]*1e-3, yC[:]*1e-3, z_interp, linewidth=1.5, color=:black, levels=10)
+
+# Third plot: Stratification
+hm3 = heatmap!(ax3, xC[:]*1e-3, yC[:]*1e-3, @lift(dropdims($Bz_slice, dims=3)),
+               colormap = :GnBu,
+               colorrange = (0, 2e-6))
+cb3 = Colorbar(layout[2, 6], hm3, width=8)  # Place in column 6, slim width
+contour!(ax3, xC[:]*1e-3, yC[:]*1e-3, z_interp, linewidth=1.5, color=:black, levels=10)
+
+# Set column widths: larger for subplots, smaller for colorbars
+colsize!(layout, 1, Relative(0.3))  # Subplot 1
+colsize!(layout, 2, Relative(0.03))  # Colorbar 1
+colsize!(layout, 3, Relative(0.3))  # Subplot 2
+colsize!(layout, 4, Relative(0.03))  # Colorbar 2
+colsize!(layout, 5, Relative(0.3))  # Subplot 3
+colsize!(layout, 6, Relative(0.03))  # Colorbar 3
+
+# Minimize spacing between columns
+colgap!(layout, 0)  # Set to 0 to eliminate gaps
+
+# Add minimal padding to figure
+fig.attributes.margin = (10, 10, 10, 10)
+
+# Create the animation
+record(fig, anim_filename, 1:length(z_position); framerate=5) do i
+    @info "Rendering frame $i of $(length(z_position))..."
+    n[] = i
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# using PyPlot
+# close(gcf())
+# PyPlot.plot(nanmean(interpolated_what_tavg[:,:,:],dim=(1,2)),bin_centers)
+# gcf()
+# savefig("output/tilt/interpolated_what_ln.png")
+
+
+# # plot only Bz 
+# using PyPlot
+
+# close(gcf())
+# fig, ax = subplots(1, 1, figsize=(5, 5))
+#     # mpl_colors = PyPlot.pyimport("matplotlib.colors")
+#     # linthresh = 1e-8
+
+#     # norm_log = mpl_colors["SymLogNorm"](linthresh=linthresh, vmin=-1e-7, vmax=1e-5, base=10)
+# c = ax.pcolor(xC[:]*1e-3, yC[:]*1e-3, (nanmean(interpolated_Bz_tavg[:,:,1:19],dim=(3))'),
+#                 cmap="RdYlBu_r",
+#                 vmin=0, vmax=2e-6)
+# colorbar(c, ax=ax, extend="both")
+# ax.set_title(L"\bar{B}_z (0-92.5 m)")
+# h=ax.contour(xC[:]*1e-3, yC[:]*1e-3, z_interp', linewidths=1.5, colors="black")
+# PyPlot.clabel(h, inline=true, fontsize=8)  # Add labels to contour lines
+# ax.set_xlabel("x [km]")
+# ax.set_ylabel("y [km]")
+# savefig("output/tilt/interpolated_Bz_heatmap_0-92.5.png")
